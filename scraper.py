66d42ee0bb88395463f24a59255b480ebdecd5f2
Johnny Yeo <johnnyyeo@Johnnys-MacBook-Pro.local>
2016-11-03 23:28:42 -0700

    first progress check of proj

--- scraper.py
+++ scraper.py
@@ -37,6 +37,33 @@ def Player_Scrape_by_Season(url):
 			rows[season.attrs['id']].append(row.text)
 	return rows
 
+def Player_Game_Log_Scrape(url):
+	"""
+	Scrape the player's game logs, return a dictionary with (key, val) => (game, stats)
+	"""
+	all_gamelogs = {}
+	response = urllib2.urlopen(url)
+	html = response.read()
+	response.close()
+	soup = BeautifulSoup(html, 'html.parser')
+	gamelogs = soup.find("li", class_="full hasmore ").find_all("a", href=True)
+	all_logs = []
+	for logs in gamelogs:
+		all_logs.append('http://www.basketball-reference.com' + logs['href']) #grab all game log links of player
+	for season_logs in all_logs:
+		response = urllib2.urlopen(season_logs)
+		html = response.read()
+		response.close()
+		soup = BeautifulSoup(html, 'html.parser')
+		logs = soup.find("div", class_="table_outer_container").find("tbody").find_all("tr")
+		for log in logs:
+			if int(log['id'].split('.')[1]) != 0:
+				date = log.find("td",{"data-stat":"date_game"}).getText()
+				all_gamelogs[date] = {}
+				for data in log:
+					all_gamelogs[date][data["data-stat"]] = data.getText() #get all stats of gamelog
+					
+
 if __name__ == "__main__":
 	player_data = {}
 	letters=map(chr, range(97, 123))
@@ -45,13 +72,31 @@ if __name__ == "__main__":
 	html = response.read()
 	response.close()
 	soup = BeautifulSoup(html, 'html.parser')
-	season_data = soup.find(id='all_totals')
-	colnames = []
-	for colname in season_data.find_all('th'):
-		colnames.append(colname.attrs['tip'])
-	colnames[0] = 'Season' #Manual Adjustments
-	colnames[1] = 'Age' #Manual Adjustments
-	colnames[17] = 'Effective Field Goal Percentage' #Manual Adjustments
+	gamelogs = soup.find("li", class_="full hasmore ").find_all("a", href=True)
+	all_logs = []
+	for logs in gamelogs:
+		all_logs.append('http://www.basketball-reference.com' + logs['href']) #grab all game log links of player
+		#player_data[PlayerID] = {'name':'Quincy Acy'} NEEDS TO BE THOUGHT OUT
+	for season_logs in all_logs:
+		response = urllib2.urlopen(season_logs)
+		html = response.read()
+		response.close()
+		soup = BeautifulSoup(html, 'html.parser')
+		logs = soup.find("div", class_="table_outer_container").find("tbody").find_all("tr")
+		for log in logs:
+
+			if int(log['id'].split('.')[1]) != 0:
+
+
+
+
+	# season_data = soup.find(id='all_totals')
+	# colnames = []
+	# for colname in season_data.find_all('th'):
+	# 	colnames.append(colname.attrs['tip'])
+	# colnames[0] = 'Season' #Manual Adjustments
+	# colnames[1] = 'Age' #Manual Adjustments
+	# colnames[17] = 'Effective Field Goal Percentage' #Manual Adjustments
 
 	##Grab Player Data
 	for letter in letters:
