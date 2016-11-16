9b8731604ccbe5cb77b5aabcd81a0be255cf1927
Johnny Yeo <johnnyyeo12@gmail.com>
2016-11-16 14:26:25 -0800

    finished player-by-player gamelog scraper

--- scraper.py
+++ scraper.py
@@ -7,35 +7,35 @@ from bs4 import BeautifulSoup
 #Create method to store season data to msql db
 #Ideas for scraping other data from html pages
 
-def Player_Scrape_by_Season(url):
-	"""
-	Scrapes a player's season-by-season stats
-	"""
-	response = urllib2.urlopen(url)
-	html = response.read()
-	response.close()
-	soup = BeautifulSoup(html, 'html.parser')
+# def Player_Scrape_by_Season(url):
+# 	"""
+# 	Scrapes a player's season-by-season stats
+# 	"""
+# 	response = urllib2.urlopen(url)
+# 	html = response.read()
+# 	response.close()
+# 	soup = BeautifulSoup(html, 'html.parser')
 
-	##Grab Season data
-	season_data = soup.find(id='all_totals')
+# 	##Grab Season data
+# 	season_data = soup.find(id='all_totals')
 
 
-	#Headers
-	colnames = []
-	for colname in season_data.find_all('th'):
-		colnames.append(colname.attrs['tip'])
+# 	#Headers
+# 	colnames = []
+# 	for colname in season_data.find_all('th'):
+# 		colnames.append(colname.attrs['tip'])
 
-	colnames[0] = 'Season' #Manual Adjustments
-	colnames[1] = 'Age' #Manual Adjustments
-	colnames[17] = 'Effective Field Goal Percentage' #Manual Adjustments
+# 	colnames[0] = 'Season' #Manual Adjustments
+# 	colnames[1] = 'Age' #Manual Adjustments
+# 	colnames[17] = 'Effective Field Goal Percentage' #Manual Adjustments
 
-	#Season-by-season numbers
-	rows = {}
-	for season in season_data.find_all('tr', id=lambda x: x and x.startswith('totals.')):
-		rows[season.attrs['id']] = []
-		for row in season_data.find(id=season.attrs['id']).find_all('td'):
-			rows[season.attrs['id']].append(row.text)
-	return rows
+# 	#Season-by-season numbers
+# 	rows = {}
+# 	for season in season_data.find_all('tr', id=lambda x: x and x.startswith('totals.')):
+# 		rows[season.attrs['id']] = []
+# 		for row in season_data.find(id=season.attrs['id']).find_all('td'):
+# 			rows[season.attrs['id']].append(row.text)
+# 	return rows
 
 def Player_Game_Log_Scrape(url):
 	"""
@@ -56,15 +56,21 @@ def Player_Game_Log_Scrape(url):
 		response.close()
 		soup = BeautifulSoup(html, 'html.parser')
 		logs = soup.find("div", class_="table_outer_container").find("tbody").find_all("tr")
+		counter = 0
 		for log in logs:
-			if int(log['id'].split('.')[1]) != 0:
-				date = log.find("td",{"data-stat":"date_game"}).getText()
-				all_gamelogs[date] = {}
-				for data in log:
-					all_gamelogs[date][data["data-stat"]] = data.getText() #get all stats of gamelog
-					
+			if log.has_attr('id'):
+				if int(log['id'].split('.')[1]) != 0:
+					date = log.find("td",{"data-stat":"date_game"}).getText()
+					all_gamelogs[date] = {}
+					for data in log:
+						all_gamelogs[date][data["data-stat"]] = data.getText() #get all stats of gamelog
+	return all_gamelogs
+	
+
+				
 
 if __name__ == "__main__":
+	Player_Game_Log_Scrape('http://www.basketball-reference.com/players/a/acyqu01.html')
 	player_data = {}
 	letters=map(chr, range(97, 123))
 	##Grab Table Columns
@@ -78,13 +84,12 @@ if __name__ == "__main__":
 		all_logs.append('http://www.basketball-reference.com' + logs['href']) #grab all game log links of player
 		#player_data[PlayerID] = {'name':'Quincy Acy'} NEEDS TO BE THOUGHT OUT
 	for season_logs in all_logs:
-		response = urllib2.urlopen(season_logs)
+		response = urllib2.urlopen(season_logs.replace("https","http"))
 		html = response.read()
 		response.close()
 		soup = BeautifulSoup(html, 'html.parser')
 		logs = soup.find("div", class_="table_outer_container").find("tbody").find_all("tr")
 		for log in logs:
-
 			if int(log['id'].split('.')[1]) != 0:
 
 
